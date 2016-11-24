830a0eb05f70b53bec7fa6075fe92a1f08b570ee
Manjeet Singh <designermanjeets@gmail.com>
2016-11-24 23:11:12 +0530

    DOM Access Works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild,Renderer} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Content } from 'ionic-angular';
 
@@ -13,26 +13,31 @@ export class HomePage {
   @ViewChild('aboutme') aboutme: any;
   @ViewChild(Content) content:Content;
 
-  constructor(public navCtrl: NavController) { }
+
+  constructor(public navCtrl: NavController, private renderer:Renderer) { }
 
     onPageScroll(event) {
-        //console.log(event.target.scrollTop);
-         console.log(aboutmeOffset + 'dsadadad');
         
-        
+        if (event.target.scrollTop>document.querySelector('.yoman').offsetTop){
+            console.log('Greater Than');
+        } else {
+            console.log('Less Than');
+        }
+     
         
     }
+
+
     ngAfterViewInit() {
         this.content.addScrollListener(this.onPageScroll);
-        let aboutmeOffset = this.aboutme.nativeElement.offsetTop;
-        console.log(this.aboutme.nativeElement.offsetTop);
+         console.log(this.renderer.selectRootElement('.yoman'));
     }
 
-        scrollToAbout(){
-            console.log('clicked');
-            this.content.scrollTo(1000, 1000, 500);
-        }
-
+    scrollToAbout(){
+        console.log('clicked');
+        this.content.scrollTo(1000, 1000, 500);
+    }
+        
 
       //Footer Form
       todo = {}
